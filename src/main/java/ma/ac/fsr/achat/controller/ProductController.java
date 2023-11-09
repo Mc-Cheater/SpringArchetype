368c765c895d035b19ba330d4f1d939a9fd341ce
mc-cheater <mcadilqarboua@gmail.com>
2023-11-09 16:21:29 +0100

    download this jobintech

--- src/main/java/ma/ac/fsr/achat/controller/ProductController.java
+++ src/main/java/ma/ac/fsr/achat/controller/ProductController.java
@@ -1,13 +1,9 @@
 package ma.ac.fsr.achat.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import ma.ac.fsr.achat.dto.ProductDto;
 import ma.ac.fsr.achat.service.IProductService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +20,10 @@ public class ProductController {
         return service.findAll();
     }
 
+    @PostMapping("/add")
+    public ProductDto newProduct(@RequestBody ProductDto dto){
+      return  service.newProduct(dto);
+// Response entity to attach status code
+        //or swagger annot
+    }
 }
--- src/main/java/ma/ac/fsr/achat/service/impl/ProductServiceImpl.java
+++ src/main/java/ma/ac/fsr/achat/service/impl/ProductServiceImpl.java
@@ -3,20 +3,33 @@ package ma.ac.fsr.achat.service.impl;
 import lombok.RequiredArgsConstructor;
 import ma.ac.fsr.achat.dto.ProductDto;
 import ma.ac.fsr.achat.mapper.ProductMapper;
+import ma.ac.fsr.achat.model.Category;
+import ma.ac.fsr.achat.model.Product;
+import ma.ac.fsr.achat.repository.CategoryRepository;
 import ma.ac.fsr.achat.repository.ProductRepository;
 import ma.ac.fsr.achat.service.IProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements IProductService {
     private final ProductRepository repository;
     private final ProductMapper mapper;
-
+    private final CategoryRepository categoryRepository;
     @Override
     public List<ProductDto> findAll() {
         return mapper.entityToDto(repository.findAll());
     }
+    @Override
+    public ProductDto newProduct(ProductDto dto){
+        Product temp=mapper.dtoToEntity(dto);
+        Optional<Category> c = categoryRepository.findById(dto.getCategory().getId());
+        c.ifPresent(temp::setCategory);
+       return mapper.entityToDto(repository.save(temp));
+
+    }
+
 }
